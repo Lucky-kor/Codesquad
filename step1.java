93b2f1eb5ee31bbb5f5f7308fef3fe5471ab5972
Lucky-kor <jungmin2056@gmail.com>
2020-12-12 16:42:12 +0900

    Constructs by pushing the string according to the value entered
    Save the received value and output a message if it does not meet the standard

---
+++ step1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class step1 {
+    public static void main(String[] args) {
+        String str;
+        int value;
+        String direction;
+        String leftWord;
+        String rightWord;
+
+        Scanner scan = new Scanner(System.in);
+        str = scan.next();                      //문자열 저장
+        value = scan.nextInt();                 //밀어낼 숫자 저장
+
+        if(-101<value && value<101){
+        }else{
+            System.out.println("-100과 100까지의 정수값을 입력해 주십시오.");
+        }
+        direction = scan.next();                //방향 저장
+
+
+    }
+}
