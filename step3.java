1a6c86ca28b694e4b01ce8d6ca514c82e5935701
Lucky-kor <jungmin2056@gmail.com>
2020-12-12 21:48:42 +0900

    Cut and print input commands one by one If the last word is "'" or "2", save it together and move to the next

--- step3.java
+++ step3.java
@@ -5,6 +5,10 @@ public class step3 {
 
         String[][] cube = new String[6][9];
         String[] color = { "B" , "W" , "O" , "G" , "Y" , "R" };
+        String cheak = null;
+        String code = null;
+        String codes = null;
+        boolean quit = false;
 
         for (int i=0; i<6; i++) {                               //큐브 초기값 입력
             for (int j=0; j<9; j++){
@@ -36,6 +40,28 @@ public class step3 {
             System.out.println();
         }
         Scanner scan = new Scanner(System.in);
+        while(!quit) {
+            System.out.println();
+            System.out.print("CUBE > ");
+            code = scan.nextLine();
+            for (int k = 0; k < code.length(); k++) {       //입력된값을 한글자씩 나누어 구분
+                if (k < code.length() - 1) {
+                    cheak = code.substring(k + 1, k + 2);   //구하는값의 뒷자리의 글자를 cheak에 저장
+                } else {                                    //맨 마지막은 비교할 필요가 없어 체크값 강제입력
+                    cheak = "";
+                }
+                if (cheak.equals("'") || cheak.equals("2")) {                    //cheak의 값이 '일경우 '를 포함해 뒤의값의값까지 함께 저장
+                    codes = code.substring(k, k + 2);
+                    k++;                                    //뒷자리 패싱
+                } else {                                    //이외의 경우 한자리만 저장
+                    codes = code.substring(k, k + 1);
+                }
+                if (codes.equals(MoveCube.getCode(codes))) {//코드값 비교후 코드 출력
+                    System.out.println();
+                    System.out.println(MoveCube.getCode(codes));
+                }
+            }
+        }
 
     }
     private enum  MoveCube {                                    //명령어 정렬
