1dad24a0e642b47c175c6159331988944bfce573
Lucky-kor <jungmin2056@gmail.com>
2020-12-13 13:56:52 +0900

    Refactoring code

--- step1.java
+++ step1.java
@@ -2,52 +2,92 @@ import java.util.Scanner;
 
 public class step1 {
     public static void main(String[] args) {
-        String str = null;
-        int value = 0;
-        String direction = null;
-        String leftWord = null;
-        String rightWord = null;
+        StringAndValueAndDirection inputValue = inputValue(); // 값 입력 받
 
+        if (mustReverseValue(inputValue.value)) { // 음수일경우 값 변경후 방향전환
+            inputValue.value = Math.abs(inputValue.value);
+            inputValue.direction = inputValue.direction.reverseWord();
+        }
+
+        while (inputValue.value > inputValue.str.length()) { // 문자열 자르기 위한 값 구하기
+            inputValue.value = inputValue.value - inputValue.str.length();
+        }
+
+        StringBuilder sb = new StringBuilder();
+        if (inputValue.direction.equals(Direction.RIGHT)) {
+            sb.append(inputValue.str.substring(inputValue.str.length() - inputValue.value));
+            sb.append(inputValue.str.substring(0, inputValue.str.length() - inputValue.value));
+        } else {
+            sb.append(inputValue.str.substring(inputValue.value));
+            sb.append(inputValue.str.substring(0, inputValue.value));
+        }
+        System.out.println(sb.toString());
+    }
+
+    private static StringAndValueAndDirection inputValue() {
+        StringAndValueAndDirection data = new StringAndValueAndDirection();
         Scanner scan = new Scanner(System.in);
+
         boolean isCorrectInput = false;
         // 제대로 입력될 때 까지 무한루프
         while (!isCorrectInput) {
             try {
-                str = scan.next();                      //문자열 저장
-                value = scan.nextInt();                 //밀어낼 숫자 저장
-                if (Math.abs(value) > 100) { // 절대값
+                data.str = scan.next();                      //문자열 저장
+                data.value = scan.nextInt();                 //밀어낼 숫자 저장
+                if (Math.abs(data.value) > 100) { // 절대값
                     System.out.println("-100과 100까지의 정수값을 입력해 주십시오.");
                     break;
                 }
-                direction = scan.next();                //방향 저장
+                data.direction = Direction.getWord(scan.next());                //방향 저장
                 isCorrectInput = true;
             } catch (Exception e) {
                 System.out.println("제대로 된 값을 입력해 주세요.");
             }
         }
+        return data;
+    }
 
-        if(value<0) {                                   //음수일경우 값 변경후 방향전환
-            value = Math.abs(value);
-            if (direction.toLowerCase().equals("r")) {
-                direction = "L";
-            } else {
-                direction = "R";
+    private static boolean mustReverseValue(int value) {
+        return value < 0;
+    }
+
+    private enum Direction {
+        LEFT("L"),
+        RIGHT("R");
+
+        private String simpleDirection;
+
+        Direction(String simpleDirection) {
+            this.simpleDirection = simpleDirection;
+        }
+
+        public static Direction getWord(String simpleDirection) {
+            for (Direction word: values()) {
+                if (word.simpleDirection.equals(simpleDirection.toUpperCase())) {
+                    return word;
+                }
+            }
+            return null;
+        }
+
+        public Direction reverseWord() {
+            switch (this) {
+                case LEFT:
+                    return RIGHT;
+                case RIGHT:
+                    return LEFT;
+                default:
+                    return null;
             }
         }
+    }
 
-        while (value > str.length()) {                   //문자열 자르기 위한 값 구하기
-            value -= str.length();
+    public static class StringAndValueAndDirection {
+        String str;
+        int value;
+        Direction direction;
+
+        public StringAndValueAndDirection() {
         }
-
-        if (direction.toLowerCase().equals("r")) {      //구한값만큼 방향 확인후 이동 대소문자 구분 명령어사용으로 정
-            leftWord = str.substring(str.length() - value);
-            rightWord = str.substring(0, str.length() - value);
-        } else {
-            leftWord = str.substring(value);
-            rightWord = str.substring(0, value);
-        }
-
-        System.out.println(leftWord + rightWord);
     }
 }
-
